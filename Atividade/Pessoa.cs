8cae10cc099b9ee8ea842115d71197258427f9d4
FelipeLMoreir <felipe.lm.moreira@gmail.com>
2025-10-14 11:29:24 -0300

    Atualizado

---
+++ Atividade/Pessoa.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade
+{
+    internal class Pessoa
+    {
+        private string Nome;
+        private string Sobrenome;
+        private int Idade;
+        private char Sexo;
+        private double Altura;
+
+        public void setNome(string nome)
+        {
+            this.Nome = nome;
+        }
+        public string getNome()
+        {
+            return this.Nome;
+        }
+        public void setSobrenome(string sobrenome)
+        {
+            this.Sobrenome = sobrenome;
+        }
+        public string getSobrenome()
+        {
+            return this.Sobrenome;
+        }
+        public void setIdade(int idade)
+        {
+            this.Idade = idade;
+        }
+        public int getIdade()
+        {
+            return this.Idade;
+        }
+        public void setSexo(char sexo)
+        {
+            this.Sexo = sexo;
+        }
+        public char getSexo()
+        {
+            return this.Sexo;
+        }
+        public void setAltura(double altura)
+        {
+            this.Altura = altura;
+        }
+        public double getAltura()
+        {
+            return this.Altura;
+        }
+
+        public void ExibirAtributos()
+        {
+            Console.WriteLine("Nome:" + this.Nome);
+            Console.WriteLine("Sobrenome:" + this.Sobrenome);
+            Console.WriteLine("Idade:" + this.Idade);
+            Console.WriteLine("Sexo:" + this.Sexo);
+            Console.WriteLine("Altura:" + this.Altura);
+        }
+    }
+}
---
+++ Atividade/Program.cs
@@ -0,0 +1,26 @@
+﻿using Atividade;
+
+Pessoa[] pessoa = new Pessoa[3];
+
+for (int i = 0; i < pessoa.Length; i++)
+{
+    pessoa[i] = new Pessoa();
+
+    Console.WriteLine($"Digite o nome da pessoa {i + 1}:");
+    pessoa[i].setNome(Console.ReadLine()!);
+    Console.WriteLine($"Digite o sobrenome da pessoa {i + 1}:");
+    pessoa[i].setSobrenome(Console.ReadLine()!);
+    Console.WriteLine($"Digite a idade da pessoa {i + 1}:");
+    pessoa[i].setIdade(int.Parse(Console.ReadLine()!));
+    Console.WriteLine($"Digite o sexo da pessoa {i + 1}(M/F):");
+    pessoa[i].setSexo(char.Parse(Console.ReadLine()!));
+    Console.WriteLine($"Digite a altura da pessoa {i + 1}:");
+    pessoa[i].setAltura(double.Parse(Console.ReadLine()!));
+    Console.Clear();
+}
+
+for (int i = 0; i < pessoa.Length; i++)
+{
+    pessoa[i].ExibirAtributos();
+}
+
